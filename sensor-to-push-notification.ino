efb65e2e8e486bc0e9ed5148501468f83585e1d9
kurimawxx00 <kurimawxx00@gmail.com>
2019-12-06 16:17:49 +0800

    Add files via upload

---
+++ sensor-to-push-notification.ino
@@ -0,0 +1,93 @@
+/*
+ *  This sketch sends push notifications via Pushbullet App through PushingBox.
+ *
+ *  You need to create a service and scenario at pushingbox.com to acquire
+ *  a device ID. Also change lines 12 and 13 to your own WiFi SSID and password
+ *
+ */
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
+const char* ssid     = "PLDTHOMEFIBRjVZpa";
+const char* password = "stacey0921";
+
+const char* host = "api.pushingbox.com";
+const char* devid = "v2EF6529B2FD9B38";
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(10);
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+int value = 0;
+
+void loop()
+{
+    delay(5000);
+    value = analogRead(A0);
+
+    if(value > 800){  //value from sensor if dry
+
+    Serial.print("connecting to ");
+    Serial.println(host);
+
+    // Use WiFiClient class to create TCP connections
+    WiFiClient client;
+    const int httpPort = 80;
+    if (!client.connect(host, httpPort)) {
+        Serial.println("connection failed");
+        return;
+    }
+
+    // We now create a URI for the request
+    String url = "/pushingbox";
+    url += "?devid=";
+    url += devid;
+    
+    Serial.print("Requesting URL: ");
+    Serial.println(url);
+
+    // This will send the request to the server
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n\r\n");
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+        if (millis() - timeout > 5000) {
+            Serial.println(">>> Client Timeout !");
+            client.stop();
+            return;
+        }
+    }
+
+    // Read all the lines of the reply from server and print them to Serial
+    while(client.available()) {
+        String line = client.readStringUntil('\r');
+        Serial.print(line);
+    }
+
+    Serial.println();
+    Serial.println("closing connection");
+    }else{;}
+}
